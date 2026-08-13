5922da8285c5931476a95bc8e03250c9062432b9
Alexey <keo564130@gmail.com>
2026-08-13 16:09:37 +0300

    fix: revert m.vk.com since it drops og tags

--- api/parse-url.js
+++ api/parse-url.js
@@ -38,12 +38,7 @@ export default async function handler(req, res) {
   // We rewrite it to `/video` which returns the correct og:description and og:image.
   if (url.includes('vk.com/clip')) {
     url = url.replace('vk.com/clip', 'vk.com/video')
-  }
 
-  // Rewrite to mobile VK to bypass "Unsupported browser" blocks
-  if (url.includes('vk.com') && !url.includes('m.vk.com')) {
-    url = url.replace('vk.com', 'm.vk.com')
-  }
 
   // For Telegram, we need to use the embed widget to get metadata
   if (url.includes('t.me/') && !url.includes('?embed=1') && !url.includes('&embed=1')) {
